55f79a6c7c4cb6d4e9bf6bb47d6ca578c54187c0
Sander <s.knape88@gmail.com>
2020-01-28 14:55:06 +0100

    debug info

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ ls -hal
 terraform init
 terraform validate
 
-data=planCommentWrapper="#### \`terraform plan\` ${planCommentStatus}
+data="#### \`terraform plan\` ${planCommentStatus}
 <details><summary>Show Output</summary>
 {
     "some": {
@@ -19,5 +19,7 @@ data=planCommentWrapper="#### \`terraform plan\` ${planCommentStatus}
 }
 </details>
 Some more text"
+
 planCommentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
-echo "${data}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${planCommentsURL}" > /dev/null
+echo "${data}"
+echo "${data}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${planCommentsURL}"
