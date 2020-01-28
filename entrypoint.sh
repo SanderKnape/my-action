152ec99279892ac5031fd048e686e1d16e0370e5
Sander <s.knape88@gmail.com>
2020-01-28 15:02:22 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+function stripColors {
+  echo "${1}" | sed 's/\x1b\[[0-9;]*m//g'
+}
+
 env
 echo "1: ${1}"
 cd ${1}
@@ -22,8 +26,11 @@ data="#### \`terraform plan\` ${planCommentStatus}
 </details>
 Some more text"
 
+data=$(stripColors "${data}")
+planPayload=$(echo "${data}" | jq -R --slurp '{body: .}')
+
 planCommentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
-echo "${data}"
+echo "${planPayload}"
 echo "${planCommentsURL}"
 
-echo "${data}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${planCommentsURL}"
+echo "${planPayload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${planCommentsURL}"
