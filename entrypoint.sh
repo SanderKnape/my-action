27af6fb5a92b7fd980974c903c37aca0051a3036
Sander <s.knape88@gmail.com>
2020-01-28 14:49:51 +0100

    test posting comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
 env
-echo "1: ${INPUT_WORKING-DIRECTORY}"
-cd ${INPUT_WORKING-DIRECTORY}
+echo "1: ${1}"
+cd ${1}
 pwd
 ls -hal
 
 terraform init
 terraform validate
+
+data=planCommentWrapper="#### \`terraform plan\` ${planCommentStatus}
+<details><summary>Show Output</summary>
+{
+    "some": {
+        "json": true
+    },
+    "even": ["more"]
+}
+</details>
+Some more text"
+planCommentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+echo "${data}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${planCommentsURL}" > /dev/null
