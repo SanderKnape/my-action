510221b9b1cd085dfd2eeb8363f92eee929de0ed
Sander <s.knape88@gmail.com>
2020-03-17 11:09:29 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-env
+export AWS_ACCESS_KEY_ID=${1}
+export AWS_SECRET_ACCESS_KEY=${2}
 
-echo $1
-echo $2
-echo $3
-echo $4
+if [ "${APP}" == "refs/heads/master"]
+    export TAG_PREFIX="master-"
+else
+    export TAG_PREFIX="branch-"
+fi
+
+export IMAGE="${DOCKER_REPO_URL}/${APP}:${TAG_PREFIX}-${TAG}"
+docker tag ${APP} ${IMAGE}
