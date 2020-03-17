4739bd500d785c9c1ba96b76ef9931e90d0913da
Sander <s.knape88@gmail.com>
2020-03-17 10:49:35 +0100

    test

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
-FROM debian:buster
+FROM debian:buster-slim
 
-RUN apt-get update && \
-    apt-get install -y curl wget && \
-    curl -sL https://deb.nodesource.com/setup_12.x | bash - && \
-    apt-get install -y nodejs && \
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
-    bash -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
-    apt-get update && \
-    apt-get install -y google-chrome-stable --no-install-recommends
+RUN apt-get update \
+    && apt-get install -y awscli docker.io \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 
-ENTRYPOINT ["./node_modules/.bin/wdio", "wdio.conf.js"]
+COPY entrypoint.sh .
+
+ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,7 @@
 #!/bin/sh
 
-function stripColors {
-  echo "${1}" | sed 's/\x1b\[[0-9;]*m//g'
-}
-
 env
-echo "1: ${1}"
-cd ${1}
-pwd
-ls -hal
 
-terraform init
-# terraform validate
-
-echo "HERE"
-
-data="#### \`terraform plan\` ${planCommentStatus}
-<details><summary>Show Output</summary>
-{
-    \"some\": {
-        \"json\": true
-    },
-    \"even\": [\"more\"]
-}
-</details>
-Some more text"
-
-data=$(stripColors "${data}")
-planPayload=$(echo "${data}" | jq -R --slurp '{body: .}')
-
-planCommentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
-echo "${planPayload}"
-echo "${planCommentsURL}"
-
-echo "${planPayload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${planCommentsURL}"
+echo $1
+echo $2
+echo $3
